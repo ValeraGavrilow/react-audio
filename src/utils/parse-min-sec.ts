032558f04aa8e370763acf38c3f0b866f51ebb1c
Valera <gavrilow777@gmail.com>
2023-08-19 18:42:54 +0300

    added some description

--- src/utils/parse-min-sec.ts
+++ src/utils/parse-min-sec.ts
@@ -1,7 +1,9 @@
 export function parseHoursMinSec(seconds: number): string {
   const milliseconds = seconds * 1000;
+  // return 'HH:MM:SS'
   if (milliseconds > 3_600_000) {
     return new Date(seconds * 1000).toISOString().slice(11, 19);
   }
+  // return 'MM:SS'
   return new Date(seconds * 1000).toISOString().slice(14, 19);
 }
